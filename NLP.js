d10db13fd14a333e88add9e53e1d664e4c91ae52
Sufyan Khan <sufyankhans072@gmail.com>
2025-02-28 20:53:51 +0000

    listed data

--- NLP.js
+++ NLP.js
@@ -17,29 +17,7 @@ dataset.forEach((data) => {
 // Train the classifier
 classifier.train();
 
-app.post("/chat", (req, res) => {
-  const { message } = req.body;
 
-  if (!message) {
-    return res.status(400).json({ error: "Message is required!" });
-  }
-
-  // Tokenize and classify the input message with probabilities
-  const classifications = classifier.getClassifications(message.toLowerCase());
-  console.log(classifications, "classifications");
-
-  // Log only the most probable classification (the top one)
-  const bestMatch = classifications[0]; // Get the most probable classification
-  console.log(`User: ${message}`);
-  console.log("Best Classification:", bestMatch); // Log only the best match
-
-  // Respond with the best classification
-  const response = bestMatch
-    ? bestMatch.label
-    : "Sorry, I don't understand that.";
-
-  res.json({ response });
-});
 
 app.listen(port, () => {
   console.log(`NLP Chatbot running at http://localhost:${port}`);
